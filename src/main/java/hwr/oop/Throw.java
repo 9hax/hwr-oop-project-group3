2ad2f6d11d0be2b0c44fd77b82cc25f7619af751
Arian Härtl <hartlarian@gmail.com>
2023-05-09 11:21:31 +0200

    added class Throw and Test for GetFallenPins

---
+++ src/main/java/hwr/oop/Throw.java
@@ -0,0 +1,12 @@
+package hwr.oop;
+
+public class Throw {
+    int fallenPins;
+    public void setFallenPins(int fallenPins) {
+        this.fallenPins = fallenPins;
+    }
+
+    public int getFallenPins() {
+        return fallenPins;
+    }
+}
--- src/test/java/hwr/oop/BowlingThrowTest.java
+++ src/test/java/hwr/oop/BowlingThrowTest.java
@@ -7,11 +7,21 @@ public class BowlingThrowTest {
     @Test
     void testSetFallenPins(){
         //given
-        Throw throw = new Throw();
+        Throw bowlThrow = new Throw();
         //when
-        throw.setFallenPins(3);
+        bowlThrow.setFallenPins(3);
         //then
-        assertThat(throw.getPins()).isEqualTo(3);
+        assertThat(bowlThrow.getFallenPins()).isEqualTo(3);
     }
 
+    @Test
+    void testGetFallenPins(){
+        //given
+        Throw bowlThrow = new Throw();
+        bowlThrow.setFallenPins(3);
+        //when
+        int pins = bowlThrow.getFallenPins();
+        //then
+        assertThat(pins).isEqualTo(3);
+    }
 }
