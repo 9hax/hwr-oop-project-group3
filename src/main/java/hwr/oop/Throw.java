5fd68e46073a901c9c8d60ab48c6cdc73d521f67
annika <ammfsee@gmail.com>
2023-05-09 11:48:18 +0200

    add exception for setFallenPins()

--- src/main/java/hwr/oop/Throw.java
+++ src/main/java/hwr/oop/Throw.java
@@ -2,8 +2,12 @@ package hwr.oop;
 
 public class Throw {
     int fallenPins;
-    public void setFallenPins(int fallenPins) {
-        this.fallenPins = fallenPins;
+    public void setFallenPins(int fallenPins) throws IllegalArgumentException{
+        if (fallenPins <= 10 && fallenPins >= 0) {
+            this.fallenPins = fallenPins;
+        } else {
+            throw new IllegalArgumentException("FALSCH! Please enter a number between 0 and 10.");
+        }
     }
 
     public int getFallenPins() {
--- src/test/java/hwr/oop/BowlingThrowTest.java
+++ src/test/java/hwr/oop/BowlingThrowTest.java
@@ -1,17 +1,39 @@
 package hwr.oop;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
 import org.junit.jupiter.api.Test;
 
 public class BowlingThrowTest {
     @Test
-    void testSetFallenPins(){
+    void testSetFallenPins_3PinsFallen(){
         //given
         Throw bowlThrow = new Throw();
         //when
         bowlThrow.setFallenPins(3);
         //then
         assertThat(bowlThrow.getFallenPins()).isEqualTo(3);
+    }
+
+    @Test
+    void testSetFallenPins_15PinsFallen(){
+        //given
+        Throw bowlThrow = new Throw();
+        //when
+
+        //then
+        assertThatThrownBy(() -> {bowlThrow.setFallenPins(15);}).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void testSetFallenPins_negativeNumber(){
+        //given
+        Throw bowlThrow = new Throw();
+        //when
+
+        //then
+        assertThatThrownBy(() -> {bowlThrow.setFallenPins(-1);}).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
