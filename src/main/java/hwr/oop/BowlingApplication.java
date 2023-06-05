1e701295bf26d94ad5f816fa79b7eafa8dcb7b0c
9hax <me@9hax.net>
2023-06-05 15:32:09 +0200

    add main function

---
+++ src/main/java/hwr/oop/BowlingApplication.java
@@ -0,0 +1,18 @@
+package hwr.oop;
+
+public class BowlingApplication {
+    public static void main(String[] args) {
+        appLifecycle(new ConsoleIOAdapter());
+    }
+
+    public static void appLifecycle(IOAdapter ioAdapter) {
+        TextUI applicationUI = new ConsoleTextUI(ioAdapter);
+        boolean playAnotherGame = true;
+        while(playAnotherGame) {
+            applicationUI.createGame();
+            applicationUI.playGame();
+            playAnotherGame = applicationUI.askRestart();
+        }
+        ioAdapter.putString("Goodbye!");
+    }
+}
---
+++ src/test/java/hwr/oop/LifecycleTest.java
@@ -0,0 +1,34 @@
+package hwr.oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+class LifecycleTest {
+    @Test
+    void testLifecycle() {
+        IOAdapter ioAdapter = new MockIOAdapter();
+        ioAdapter.queueInput("Alex");
+        ioAdapter.queueInput("");
+
+        ioAdapter.pollOutput();
+        for(int round = 0; round< 20; round++){
+            ioAdapter.queueInput("3");
+        }
+        ioAdapter.queueInput("Y"); // Play another game! YYAYY!!!! slay~
+
+        ioAdapter.queueInput("Sbeve");
+        ioAdapter.queueInput("");
+
+        ioAdapter.pollOutput();
+        for(int round = 0; round< 20; round++){
+            ioAdapter.queueInput("2");
+        }
+        ioAdapter.queueInput("N"); // Don't play another game.
+
+        BowlingApplication.appLifecycle(ioAdapter);
+        ioAdapter.trimOutputQueue(3);
+        assertThat(ioAdapter.pollOutput()).isEqualTo("Sbeve scored 40 points.");
+        assertThat(ioAdapter.pollOutput()).isEqualTo("Input Y to play another game. \n>");
+        assertThat(ioAdapter.pollOutput()).isEqualTo("Goodbye!");
+    }
+}
