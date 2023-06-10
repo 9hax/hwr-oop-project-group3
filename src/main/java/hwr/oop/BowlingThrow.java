3f7f436acf76a14ece3431012dcfedc015fbee7a
Anna Stabel <anna.stabel@sap.com>
2023-06-10 20:38:16 +0200

    bowlingThrow split implemented

--- src/main/java/hwr/oop/BowlingThrow.java
+++ src/main/java/hwr/oop/BowlingThrow.java
@@ -1,14 +1,34 @@
 package hwr.oop;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class BowlingThrow {
+    private String fallenPinsString;
     private int fallenPins;
-    public BowlingThrow(int fallenPins) {
+    public BowlingThrow(String fallenPinsString) {
+        setFallenPins(fallenPinsString);
+    }
+    public BowlingThrow(Integer fallenPins) {
         setFallenPins(fallenPins);
     }
+
     public BowlingThrow() {
     }
 
-    public void setFallenPins(int fallenPins) throws IllegalArgumentException{
+    public void setFallenPins(String fallenPinsString) throws IllegalArgumentException{
+        if(fallenPinsString.equals("-")){
+            this.fallenPinsString = fallenPinsString;
+        }
+        if (convertAndSortList(fallenPinsString).get(0)<=10 && convertAndSortList(fallenPinsString).get(0) >= 0) {
+            this.fallenPinsString = fallenPinsString;
+        } else {
+            throw new IllegalArgumentException("FALSCH! Please enter a number between 0 and 10.");
+        }
+    }
+    public void setFallenPins(Integer fallenPins) throws IllegalArgumentException{
         if (fallenPins <= 10 && fallenPins >= 0) {
             this.fallenPins = fallenPins;
         } else {
@@ -17,10 +37,27 @@ public class BowlingThrow {
     }
 
     public int getFallenPins() {
-        return fallenPins;
+        if (fallenPinsString == null){
+            return fallenPins;
+        }
+        else{
+            return convertAndSortList(fallenPinsString).size();
+        }
+    }
+    public String getFallenPinsString() {
+        return fallenPinsString;
+    }
+    public boolean hasClearedString() {
+        return Objects.equals(fallenPins, "1 2 3 4 5 6 7 8 9 10");
+    }
+    public boolean hasCleared() {
+        return fallenPins==10;
     }
 
-    public boolean hasCleared() {
-        return fallenPins == 10;
+    private static List<Integer> convertAndSortList(String scan){
+        if(scan.equals("-")){
+            return List.of(0);
+        }
+        return Arrays.stream(scan.split(" ")).map(Integer::parseInt).sorted().collect(Collectors.toList());
     }
 }
--- src/test/java/hwr/oop/BowlingBowlingThrowTest.java
+++ src/test/java/hwr/oop/BowlingBowlingThrowTest.java
@@ -5,9 +5,11 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+
 class BowlingBowlingThrowTest {
     @Test
-    void testSetFallenPins_ValidPinsFallen(){
+    void testSetFallenPinsInt_ValidPinsFallen(){
         //given
         BowlingThrow bowlThrow = new BowlingThrow();
 
@@ -21,7 +23,7 @@ class BowlingBowlingThrowTest {
     }
 
     @Test
-    void testSetFallenPins_InvalidPinsFallen(){
+    void testSetFallenPinsInt_InvalidPinsFallen(){
         //given
         BowlingThrow bowlThrow = new BowlingThrow();
         //then
@@ -32,7 +34,7 @@ class BowlingBowlingThrowTest {
     }
 
     @Test
-    void testSetFallenPins_negativeNumber(){
+    void testSetFallenPinsInt_negativeNumber(){
         //given
         BowlingThrow bowlThrow = new BowlingThrow();
         //when
@@ -40,9 +42,8 @@ class BowlingBowlingThrowTest {
         //then
         assertThatThrownBy(() -> bowlThrow.setFallenPins(-1)).isInstanceOf(IllegalArgumentException.class);
     }
-
     @Test
-    void testGetFallenPins(){
+    void testGetFallenPinsInt(){
         //given
         BowlingThrow bowlThrow = new BowlingThrow();
         bowlThrow.setFallenPins(3);
@@ -50,5 +51,49 @@ class BowlingBowlingThrowTest {
         int pins = bowlThrow.getFallenPins();
         //then
         assertThat(pins).isEqualTo(3);
+    }
+
+    @Test
+    void testGetFallenPinsString(){
+        //given
+        BowlingThrow bowlThrow = new BowlingThrow();
+        bowlThrow.setFallenPins("3");
+        //when
+        String pins = bowlThrow.getFallenPinsString();
+        //then
+        assertThat(pins).isEqualTo("3");
+    }
+    @Test
+    void testSetFallenPinsString_ValidPinsFallen(){
+        //given
+        BowlingThrow bowlThrow = new BowlingThrow();
+
+        bowlThrow.setFallenPins("3");
+        assertThat(bowlThrow.getFallenPinsString()).isEqualTo("3");
+
+        bowlThrow.setFallenPins("-");
+        assertThat(bowlThrow.getFallenPinsString()).isEqualTo("-");
+        bowlThrow.setFallenPins("10");
+        assertThat(bowlThrow.getFallenPinsString()).isEqualTo("10");
+    }
+
+    @Test
+    void testSetFallenPinsString_InvalidPinsFallen(){
+        //given
+        BowlingThrow bowlThrow = new BowlingThrow();
+        //then
+        assertThatThrownBy(() -> bowlThrow.setFallenPins("15")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> bowlThrow.setFallenPins("-1")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> bowlThrow.setFallenPins("11")).isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @Test
+    void getPointsFromFallenPinsString_Test() {
+        BowlingThrow bowlThrow = new BowlingThrow();
+
+        bowlThrow.setFallenPins("3");
+        assertThat(bowlThrow.getFallenPins()).isEqualTo(1);
+
     }
 }
